7e89378b5cb6e832795600e5e3cb672feda52478
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-04 09:31:11 +0900

    finished calling gif array in gif list with keyup

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-const SearchBar = changeGifIds => {
+const SearchBar = ({ changeGifIds }) => {
   const handleKeyUp = event => {
-    // changeGifIds();
-    console.log(event.currentTarget.value);
+    changeGifIds(event.currentTarget.value);
   };
 
   return <input className="form-control form-search" onKeyUp={handleKeyUp} />;
